997787d5c38fd9b46048c2bbe29a16d1e59f1ffc
giridathac <giridathac@gmail.com>
2026-01-03 18:03:12 +0530

    prod related issues

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 // vite.config.js
+import { defineConfig, loadEnv } from 'vite';
 import { fileURLToPath, URL } from 'node:url'
-import { defineConfig } from 'vite'
 import { VitePWA } from 'vite-plugin-pwa';
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
@@ -8,49 +8,53 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
 
-export default defineConfig({
-  plugins: [
-	  vue(),
-	  VitePWA({
-		registerType: 'autoUpdate', // This helps to automatically update the service worker
-		manifest: {
-			name: 'My PWA Vue App',
-			short_name: 'VuePWA',
-			theme_color: '#42b883',
-			// Other manifest options...
-			icons: [
-				{
-					src: 'pwa-192x192.png',
-					sizes: '192x192',
-					type: 'image/png',
-				},
-				{
-					src: 'pwa-512x512.png',
-					sizes: '512x512',
-					type: 'image/png',
-				},
-			],
-	    },
-	  }),
+export default defineConfig(({mode}) => {
+  const env = loadEnv(mode, process.cwd(), '');
 
-	  tailwindcss(),
-	  vueDevTools(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+  return {
+    plugins: [
+      vue(),
+      VitePWA({
+      registerType: 'autoUpdate', // This helps to automatically update the service worker
+      manifest: {
+        name: 'My PWA Vue App',
+        short_name: 'VuePWA',
+        theme_color: '#42b883',
+        // Other manifest options...
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+        },
+      }),
+
+      tailwindcss(),
+      vueDevTools(),
+    ],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      },
     },
-  },
-  
-  server: {    
-    proxy: {
-      '/api/v1': {
-        target: 'http://localhost:8080',
-        changeOrigin: true,
-        rewrite: path => path // Simply pass through the path as is        
+    
+    server: {    
+      proxy: {
+        '/api/v1': {
+          target: env.VITE_API_LOCALHOST_URL,
+          changeOrigin: true,
+          rewrite: path => path // Simply pass through the path as is        
+        }
       }
     }
-  }  
+  }
 });
 
 /*
